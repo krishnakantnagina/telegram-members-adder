6da5287b11690e5a17a64d0ec80b0a3e45d6131e
Krishnakant Nagina <85604967+krishnakantnagina@users.noreply.github.com>
2023-03-19 20:44:24 +0530

    update

---
+++ README.md
@@ -0,0 +1,144 @@
+<details><summary><h3>ABOUT😎</h3></summary>
+
+[![](https://img.shields.io/badge/TelegramMembersAdder-v1.0-darkgreen)](#)
+![Download](https://img.shields.io/github/downloads/saifalisew1508/Telegram-Members-Adder/total?style=social)
+[![Fork](https://img.shields.io/github/forks/saifalisew1508/Telegram-Members-Adder?style=social)](https://github.com/saifalisew1508/Telegram-Members-Adder/fork)
+[![Stars](https://img.shields.io/github/stars/saifalisew1508/Telegram-Members-Adder?style=social)](https://github.com/saifalisew1508/Telegram-Members-Adder/stargazers)
+![Languages](https://img.shields.io/github/languages/count/saifalisew1508/Telegram-Members-Adder?style=social)
+[![Release](https://img.shields.io/github/release-date/saifalisew1508/Telegram-Members-Adder?style=social)](https://github.com/saifalisew1508/Telegram-Members-Adder)   
+![Size](https://img.shields.io/github/languages/code-size/saifalisew1508/Telegram-Members-Adder?style=social)
+![Python](https://img.shields.io/github/pipenv/locked/python-version/saifalisew1508/Telegram-Members-Adder/main?style=social)
+[![Contributors](https://img.shields.io/github/contributors/saifalisew1508/Telegram-Members-Adder?style=social)](https://github.com/saifalisew1508/Telegram-Members-Adder/graphs/contributors)
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=social)](https://makeapullrequest.com)  
+</p>
+</details>
+
+
+<details><summary><h3>OTHER TELEGRAM TOOLS😎</h3></summary>
+  
+   <p align="center"><a href="https://github.com/saifalisew1508/TELEGRAM-MASS-DM-BOT"><img src="https://github-readme-stats.vercel.app/api/pin?username=saifalisew1508&show_icons=true&theme=dracula&hide_border=true&repo=TELEGRAM-MASS-DM-BOT"></a></p>
+
+<p align="center">
+
+
+   <p align="center"><a href="https://github.com/saifalisew1508/TelegramAdder"><img src="https://github-readme-stats.vercel.app/api/pin?username=saifalisew1508&show_icons=true&theme=dracula&hide_border=true&repo=TelegramAdder"></a></p>
+
+<p align="center">
+
+   <p align="center"><a href="https://github.com/saifalisew1508/TelegramScraper"><img src="https://github-readme-stats.vercel.app/api/pin?username=saifalisew1508&show_icons=true&theme=dracula&hide_border=true&repo=TelegramScraper"></a></p>
+
+<p align="center">
+
+</details>
+
+
+# Version 2.0
+
+* Adds accounts via only phone numbers (no need api) . If you want to add accounts only via phone and api, then scroll to the end. If you want more free projects like this, then please support me!
+
+# Update Features
+
+- Fixed bug with scraping. You can now scrape from public groups of any size.
+- Fixed bug with add.py. You can now start adding by IDs again.
+
+# Features
+
+* ADDS IN BULK
+* Scrapes and adds to public groups
+* Adds 40-50 members on an average
+* Works in Windows systems
+* You can run unlimited accounts at the same time in order to add members
+* CSV files auto-distributer based on number of accounts to use
+* Powerful scraping tool that can scrape active members from any public group
+* Least chances of account ban
+* Script auto-joins public group from all accounts for faster adding
+* Filters banned accounts and remove them, making things easy
+* It can also store unlimited accounts for adding purposes
+* Adding scripts launches automatically based on number of accounts to use
+
+# How to use
+
+<b>Suggestion: enter 15<30 delay time.</b>
+
+* First Open Termux App
+
+```
+apt update && apt upgrade && pkg install -y git python && pkg update && pkg upgrade && pkg install python git && git clone https://github.com/saifalisew1508/Telegram-Members-Adder.git
+```
+
+```
+cd Telegram-Members-Adder && ls
+```
+
+```
+pip install -r requirements.txt
+```
+
+```
+python3 -m pip install --upgrade pip
+```
+
+```
+pip install -r requirements.txt
+```
+
+
+```
+ls
+```
+
+* Installation done
+
+* Now run script and login your phone numbers by
+
+```
+python manager.py
+```
+
+
+* Aur then Adding Members By using 
+
+```
+python add.py
+```
+
+
+
+# Note
+
+Sometimes users may not get added at all, this may be because the account is probably limited or you entered a channel. Try with another account. 
+
+## 💰 Support Me
+
+<a href="https://ko-fi.com/saifalisew1508" class="padded"><img height="30" style="border:0px;height:30px;" align="left" alt="saifalisew1508" src="https://az743702.vo.msecnd.net/cdn/kofi3.png?v=0" /></a>
+<a href="https://www.paypal.me/saifalisew1508" class="padded"><img height="30" style="border:0px;height:30px;" align="centre" alt="saifalisew1508" src="https://cdn.rawgit.com/twolfson/paypal-github-button/1.0.0/dist/button.svg" /></a>
+
+# Warning
+
+* This tool is official and completely free to use. Do not buy if anyone tries to sell by copying script
+
+# Contacts
+
+* I have also created a scraper and adder which lets you add accounts via phone number, api_id and api_hash. If you need it contact me:
+
+[![Telegram Contact](https://img.shields.io/badge/Telegram-Contact-brightgreen)](https://t.me/saifalisew15O8) 
+[![Telegram Contact](https://img.shields.io/badge/Telegram-Contact-brightgreen)](https://t.me/PrinceXofficial) 
+* If you have any problems regarding the script then write it in this groupchat:
+
+[![Telegram Group](https://img.shields.io/badge/Telegram-Group-brightgreen)](https://t.me/PublicSource_Chat)
+[![Telegram Channel](https://img.shields.io/badge/Telegram-Channel-brightgreen)](https://t.me/ABOUT_SAIF)
+
+
+<h2 align="center">Visitors Counts👀</h2>
+<a href="https://github.com/saifalisew1508/Telegram-Members-Adder"><img alt="Cute Count" src="https://count.getloli.com/get/@Telegram-Members-Adder?theme=rule34" /></a>
+
+### Contribution
+- Fork this repo.
+- Add new features.
+- Create a new pull request for this branch.
+
+<h1 align="center">⚠️ALERT⚠️</h1>
+
+> Disclaimer⚠️  Please Note that this is a research project. I am by no means responsible for any usage of this tool. Use on your own behalf. I'm also not responsible if your accounts get banned due to extensive use of this tool
+
+> Scam Alert⚠️  Some scammers are selling this free script in paid and scamming people's for make money. You don't need to pay anyone for any script, if you want any script in free of cost, you can message me on Whatsapp, Telegram, Instagram and Gmail which mentioned on my git profile. and i never message you for selling anything</samp></p>
---
+++ manager.py
@@ -0,0 +1,185 @@
+from telethon.sync import TelegramClient
+from telethon.errors.rpcerrorlist import PhoneNumberBannedError
+import pickle, os
+from colorama import init, Fore
+from time import sleep
+
+init()
+
+n = Fore.RESET
+lg = Fore.LIGHTGREEN_EX
+r = Fore.RED
+w = Fore.WHITE
+cy = Fore.CYAN
+ye = Fore.YELLOW
+colors = [lg, r, w, cy, ye]
+
+try:
+    import requests
+except ImportError:
+    print(f'{lg}[i] Installing module - requests...{n}')
+    os.system('pip install requests')
+
+def banner():
+    import random
+    # fancy logo
+    b = [
+    '░██████╗███████╗████████╗██╗░░░██╗██████╗░',
+    '██╔════╝██╔════╝╚══██╔══╝██║░░░██║██╔══██╗',
+    '╚█████╗░█████╗░░░░░██║░░░██║░░░██║██████╔╝',
+    '░╚═══██╗██╔══╝░░░░░██║░░░██║░░░██║██╔═══╝░',
+    '██████╔╝███████╗░░░██║░░░╚██████╔╝██║░░░░░',
+    '╚═════╝░╚══════╝░░░╚═╝░░░░╚═════╝░╚═╝░░░░░'
+    ]
+    for char in b:
+        print(f'{random.choice(colors)}{char}{n}')
+    print('===========TELEGRAM MEMBERS ADDER============')
+
+def clr():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+while True:
+    clr()
+    banner()
+    print(f'{lg}[1] Add new accounts{n}')
+    print(f'{lg}[2] Filter all banned accounts{n}')
+    print(f'{lg}[3] Delete specific accounts{n}')
+    print(f'{lg}[4] Update your Script{n}')
+    print(f'{lg}[5] Display All Accounts{n}')
+    print(f'{lg}[6] Quit{n}')
+    a = int(input('\nEnter your choice: '))
+    if a == 1:
+        new_accs = []
+        with open('vars.txt', 'ab') as g:
+            number_to_add = int(input(f'\n{lg} [~] Enter How Many Accounts You Want To Add: {r}'))
+            for _ in range(number_to_add):
+                phone_number = str(input(f'\n{lg} [~] Enter Phone Number With Country Code: {r}'))
+                parsed_number = ''.join(phone_number.split())
+                pickle.dump([parsed_number], g)
+                new_accs.append(parsed_number)
+            print(f'\n{lg} [i] Saved all accounts in vars.txt')
+            clr()
+            print(f'\n{lg} [*] Logging in from new accounts\n')
+            for number in new_accs:
+                c = TelegramClient(f'sessions/{number}', 3910389 , '86f861352f0ab76a251866059a6adbd6')
+                c.start(number)
+                print(f'{lg}[+] Login successful')
+                c.disconnect()
+            input(f'\n Press enter to goto main menu...')
+
+        g.close()
+    elif a == 2:
+        accounts = []
+        banned_accs = []
+        with open('vars.txt', 'rb') as h:
+            while True:
+                try:
+                    accounts.append(pickle.load(h))
+                except EOFError:
+                    break
+        if not accounts:
+            print(f'{r}[!] There are no accounts! Please add some and retry')
+            sleep(3)
+        else:
+            for account in accounts:
+                phone = str(account[0])
+                client = TelegramClient(f'sessions/{phone}', 3910389 , '86f861352f0ab76a251866059a6adbd6')
+                client.connect()
+                if not client.is_user_authorized():
+                    try:
+                        client.send_code_request(phone)
+                        #client.sign_in(phone, input('[+] Enter the code: '))
+                        print(f'{lg}[+] {phone} is not banned{n}')
+                    except PhoneNumberBannedError:
+                        print(r + phone + ' is banned!' + n)
+                        banned_accs.append(account)
+            if not banned_accs:
+                print(f'{lg}Congrats! No banned accounts')
+            else:
+                for m in banned_accs:
+                    accounts.remove(m)
+                with open('vars.txt', 'wb') as k:
+                    for a in accounts:
+                        Phone = a[0]
+                        pickle.dump([Phone], k)
+                k.close()
+                print(f'{lg}[i] All banned accounts removed{n}')
+            input('\nPress enter to goto main menu...')
+    elif a == 3:
+        accs = []
+        with open('vars.txt', 'rb') as f:
+            while True:
+                try:
+                    accs.append(pickle.load(f))
+                except EOFError:
+                    break
+        print(f'{lg}[i] Choose an account to delete\n')
+        for i, acc in enumerate(accs):
+            print(f'{lg}[{i}] {acc[0]}{n}')
+        index = int(input(f'\n{lg}[+] Enter a choice: {n}'))
+        phone = str(accs[index][0])
+        session_file = f'{phone}.session'
+        if os.name == 'nt':
+            os.system(f'del sessions\\{session_file}')
+        else:
+            os.system(f'rm sessions/{session_file}')
+        del accs[index]
+        with open('vars.txt', 'wb') as f:
+            for account in accs:
+                pickle.dump(account, f)
+            print(f'\n{lg}[+] Account Deleted{n}')
+            input(f'\nPress enter to goto main menu...')
+    elif a == 4:
+        # thanks to github.com/th3unkn0n for the snippet below
+        print(f'\n{lg}[i] Checking for updates...')
+        try:
+           #  https://raw.githubusercontent.com/saifalisew1508/Telegram-Members-Adder/main/version.txt
+            version = requests.get('https://raw.githubusercontent.com/saifalisew1508/Telegram-Members-Adder/main/version.txt')
+        except:
+            print(f'{r} You are not connected to the internet')
+            print(f'{r} Please connect to the internet and retry')
+            exit()
+        if float(version.text) > 2.0:
+            prompt = str(input(f'{lg}[~] Update available[Version {version.text}]. Download?[y/n]: {r}'))
+            if prompt in {'y', 'yes', 'Y'}:
+                print(f'{lg}[i] Downloading updates...')
+                if os.name == 'nt':
+                    os.system('del add.py')
+                    os.system('del manager.py')
+                else:
+                    os.system('rm add.py')
+                    os.system('rm manager.py')
+                #os.system('del scraper.py')
+                os.system('curl -l -O https://raw.githubusercontent.com/saifalisew1508/Telegram-Members-Adder/main/add.py')
+                os.system('curl -l -O https://raw.githubusercontent.com/saifalisew1508/Telegram-Members-Adder/main/manager.py')
+                print(f'{lg}[*] Updated to version: {version.text}')
+                input('Press enter to exit...')
+                exit()
+            else:
+                print(f'{lg}[!] Update aborted.')
+                input('Press enter to goto main menu...')
+        else:
+            print(f'{lg}[i] Your Telegram-Members-Adder is already up to date')
+            input('Press enter to goto main menu...')
+    elif a == 5:
+        accs = []
+        with open('vars.txt', 'rb') as f:
+            while True:
+                try:
+                    accs.append(pickle.load(f))
+                except EOFError:
+                    break
+        print(f'\n{cy}')
+        print(f'\tList Of Phone Numbers Are')
+        print('==========================================================')
+        for z in accs:
+            print(f'\t{z[0]}')
+        print('==========================================================')
+        input('\nPress enter to goto main menu')
+    elif a == 6:
+        clr()
+        banner()
+        exit()
